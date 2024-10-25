30577ce75a4d4a30d15030905ee565c64201c3ae
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2024-10-25 14:48:02 +0330

    Add files via upload

---
+++ ConsoleApp189/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Array;
+using System.Collections;
+
+namespace ConsoleApp189
+{
+    class Program
+    {
+        private static int[] intValues = { 1, 2, 3, 4, 5, 6 };
+        private static double[] doubleValues = { 8.4, 9.3, 0.2, 7.9, 3.4 };
+        private static int[] intValuesCopy;
+        static void Main(string[] args)
+        {
+            intValuesCopy = new int[intValues.Length];
+
+            Console.WriteLine("Initial array values:\n");
+
+            PrintArray();
+
+            Sort(doubleValues);
+
+            Array.Copy(intValues, intValuesCopy, intValues.Length);
+        }
+    }
+    }
+
